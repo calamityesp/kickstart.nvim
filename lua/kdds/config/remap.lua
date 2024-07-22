30d21d6d66852dd4ba33577b58bca7cf28a13103
calamityesp <calamity_xemus@hotmail.com>
2024-07-21 23:37:50 -0600

    adding some functionality to navigate tmux panes better

--- lua/kdds/config/remap.lua
+++ lua/kdds/config/remap.lua
@@ -60,10 +60,16 @@ keymap.set("n", "<leader>ccc", "cc", { desc = "clear whole line" })
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-keymap.set("n", "<C-j>", "<C-w><C-k>", { desc = "Move focus to the lower window" })
-keymap.set("n", "<C-k>", "<C-w><C-j>", { desc = "Move focus to the upper window" })
+-- keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+-- keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+-- keymap.set("n", "<C-j>", "<C-w><C-k>", { desc = "Move focus to the lower window" })
+-- keymap.set("n", "<C-k>", "<C-w><C-j>", { desc = "Move focus to the upper window" })
+
+--  See `:help wincmd` for a list of all window commands
+keymap.set("n", "<C-Left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+keymap.set("n", "<C-Right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+keymap.set("n", "<C-Down>", "<C-w><C-k>", { desc = "Move focus to the lower window" })
+keymap.set("n", "<C-Up>", "<C-w><C-j>", { desc = "Move focus to the upper window" })
 
 -- Split window keymaps
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split Screen Vertically" }) -- split screen vertically
